2e3a9e58bd3c7f172c969600783bd20a5c83c5c3
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-01 09:18:19 -0500

    added core data.

---
+++ Reminders-SwiftUI/AppDelegate.swift
@@ -0,0 +1,25 @@
+
+import CoreData
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  // MARK: - UISceneSession Lifecycle
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+  
+  // Mark: - Core Data Stack -
+  lazy var persistantContainer: NSPersistentContainer = {
+    let container = NSPersistentContainer(name: "Reminders")
+    container.loadPersistentStores { (storeDescription, error) in
+      if let error = error as NSError? {
+        fatalError("Unresolved error \(error), \(error.userInfo)")
+      }
+    }
+    
+    return container
+  }()
+}
